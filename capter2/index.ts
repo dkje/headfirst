e3606e553faa0c3b45c2f0f248deed7ba006fff5
EbookReader <jekim@whatap.io>
2022-10-13 14:54:21 +0900

    chore 미사용 파일 삭제

--- capter2/index.ts
+++ capter2/index.ts
@@ -5,7 +5,6 @@ import WeatherData from "./WeatherData/WeatherData";
 	const weatherData = new WeatherData();
 	new CurrentConditionsDisplay({ weatherData });
 
-	console.log("뭐임..");
 	weatherData.setMeasurements({ temperature: 33, humidity: 50, pressure: 30 });
 	weatherData.setMeasurements({ temperature: 35, humidity: 670, pressure: 21 });
 })();
